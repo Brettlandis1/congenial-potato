b97b7b1e27e3cf805590bde53c7c9d539913f5cf
Scott Scherzer <scott.sch3rz3r@gmail.com>
2022-11-06 21:06:02 -0500

    functions

---
+++ 4-functions/1-basic.py
@@ -0,0 +1,15 @@
+''' 
+a function is a great way to store a block of code for re use
+this makes your software much cleaner
+Note: sometimes you may hear the word function and method interchangeably
+'''
+
+firstName = 'Jon'
+lastName = 'Doe'
+
+# use def to create a function
+def sayHello():
+  print("Hello", firstName, lastName, "!")
+
+# remeber after you create a function you must CALL it
+sayHello()  
