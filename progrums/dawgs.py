743c1908f6ff9d5b9122b77eb819799c8013d5a3
Brettlandis1 <brettxlandis@gmail.com>
2022-11-14 19:24:58 -0500

    Dawgs.py
    Added all dawgs names, age, weight,  fighting ability

--- progrums/dawgs.py
+++ progrums/dawgs.py
@@ -10,15 +10,47 @@ class Dawg:
     def sayHello(self):
         print("Hello", self.name, "age:", self.age, "weight:", self.weight)
 
-
 # create a dawg named dexter, with age 8 and weighs 125LBS
-list = [dash, dexter, robert, harold, steven, hubert, guadaloopey, bill, hoss]
 dexter = Dawg("Dexter", 8, 125, 81)
 dash = Dawg("dash", 2, 65, 91)
 robert = Dawg('robert', 5, 63, 94)
 harold = Dawg('harold', 9, 25, 35)
 steven = Dawg('steven', 5, 165, 57)
 hubert = Dawg('hubert', 11, 95, 15)
-guadaloopey = Dawg("guadaloopey", 1, 45, 99)
 bill = Dawg('bill', 11, 95, 82)
-hoss = Dawg('hoss', 8, 25, 11)            
+
+#Dexter
+print(dexter.name)
+print(dexter.age)
+print(dexter.weight)
+print(dexter.fightingAbility)
+#dash
+print(dash.name)
+print(dash.age)
+print(dash.weight)
+print(dash.fightingAbility)
+#robert
+print(robert.name)
+print(robert.age)
+print(robert.weight)
+print(robert.fightingAbility)
+#harold
+print(harold.name)
+print(harold.age)
+print(harold.weight)
+print(harold.fightingAbility)
+#steven
+print(steven.name)
+print(steven.age)
+print(steven.weight)
+print(steven.fightingAbility)
+#hubert
+print(hubert.name)
+print(hubert.age)
+print(hubert.weight)
+print(hubert.fightingAbility)
+#bill
+print(bill.name)
+print(bill.age)
+print(bill.weight)
+print(bill.fightingAbility)
