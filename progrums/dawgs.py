b115ac207ad26eda037f4808dfcf22f46a1aa9d6
Brettlandis1 <brettxlandis@gmail.com>
2022-11-15 20:50:19 -0500

    dawgs.py
    created a list but it did not work properly

--- progrums/dawgs.py
+++ progrums/dawgs.py
@@ -18,49 +18,11 @@ harold = Dawg('harold', 9, 25, 35)
 steven = Dawg('steven', 5, 165, 57)
 hubert = Dawg('hubert', 11, 95, 15)
 bill = Dawg('bill', 11, 95, 82)
+#why isnt this working grandpa????
+Dawgies = [dexter, dash, robert, harold, steven, hubert, bill]
+print(Dawgies)
 
-#Dexter
-print(dexter.name)
-print(dexter.age)
-print(dexter.weight)
-print(dexter.fightingAbility)
-#dash
-print(dash.name)
-print(dash.age)
-print(dash.weight)
-print(dash.fightingAbility)
-#robert
-print(robert.name)
-print(robert.age)
-print(robert.weight)
-print(robert.fightingAbility)
-#harold
-print(harold.name)
-print(harold.age)
-print(harold.weight)
-print(harold.fightingAbility)
-#steven
-print(steven.name)
-print(steven.age)
-print(steven.weight)
-print(steven.fightingAbility)
-#hubert
-print(hubert.name)
-print(hubert.age)
-print(hubert.weight)
-print(hubert.fightingAbility)
-#bill
-print(bill.name)
-print(bill.age)
-print(bill.weight)
-print(bill.fightingAbility)
 
 #round 1
 #dexter vs dash
-print('dexter vs dash')
-print('the winner is....')
-#what'd I do wrong on this?????
-if Dawg(dexter > dash):
-    print('dexter')
-elif Dawg(dexter > dash):
-    print('dash')
+print('dexter vs dash', 'The winner is.....')
